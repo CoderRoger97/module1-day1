2f93584d1549a933b8c46e85f1f026cd56d689b6
CoderRoger97 <humoroussiblings@gmail.com>
2021-10-03 01:32:45 +0530

    components part 1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import logo from './logo.svg';
 import {Navbar ,NavbarBrand} from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent'
 
 function App() {
   return (
     <div className="App">
       <Navbar dark color="primary">
         <div className="container">
-        
           <NavbarBrand href="/">Title of the Navbar</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
